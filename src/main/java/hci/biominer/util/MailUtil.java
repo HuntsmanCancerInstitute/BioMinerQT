9cad4b93e79531da8a95673ba6a61aceddeed457
tmaness <tmaness@86439291-5e3c-406c-9808-073398cf6135>
2014-11-13 19:37:32 +0000

    Checking commit
    git-svn-id: svn://svn.code.sf.net/p/biominerqt/code-0/trunk@118 86439291-5e3c-406c-9808-073398cf6135

---
+++ src/main/java/hci/biominer/util/MailUtil.java
@@ -0,0 +1,41 @@
+package hci.biominer.util;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public final class MailUtil {
+
+
+	public static String sendMail(String recipient, String body, String subject) {
+		String status = "";
+		String from = "DoNotReply@hci.utah.edu";
+
+		Properties properties = System.getProperties();
+
+		properties.put("mail.smtp.host", "hci-mail.hci.utah.edu");
+		Session session = Session.getDefaultInstance(properties);
+
+		try {
+			MimeMessage message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(from));
+			message.addRecipient(Message.RecipientType.TO,new InternetAddress(recipient));
+			message.setSubject(subject);
+			message.setText(body);
+			Transport.send(message);
+			status = null;
+		} catch (MessagingException mex) {
+			status =  mex.toString();
+		}
+		
+		return status;
+
+	}
+
+
+}
