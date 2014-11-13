c65919983ec98f6eb95eb652588eac87776d8e22
tmaness <tmaness@86439291-5e3c-406c-9808-073398cf6135>
2014-11-13 18:57:59 +0000

    Added /resetpassword /changepassword functionality
    git-svn-id: svn://svn.code.sf.net/p/biominerqt/code-0/trunk@110 86439291-5e3c-406c-9808-073398cf6135

--- src/main/java/hci/biominer/controller/SecurityController.java
+++ src/main/java/hci/biominer/controller/SecurityController.java
@@ -1,13 +1,17 @@
 package hci.biominer.controller;
 
+import java.sql.Timestamp;
 import java.util.Date;
+import java.util.UUID;
 
 import javax.annotation.PostConstruct;
 
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.session.Session;
+import org.apache.shiro.util.SimpleByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,16 +19,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import hci.biominer.dao.UserDAO;
 import hci.biominer.model.access.User;
 import hci.biominer.service.UserService;
 import hci.biominer.util.BiominerProperties;
 import hci.biominer.util.LoginModel;
+import hci.biominer.util.MailUtil;
+
+import java.security.SecureRandom;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+import java.math.BigInteger;
+
+
 
 @Controller
 @RequestMapping("security")
 public class SecurityController {
 	@Autowired
 	UserService userService;
+	
+	@Autowired
+	private UserController userController;
 	
 	private Long timeout = null;
 	
@@ -62,6 +80,137 @@ public class SecurityController {
 		return lm;
 	}
 	
+	@RequestMapping(value="/resetpassword",method=RequestMethod.POST)
+	public @ResponseBody String resetpassword(@RequestParam(value="username") String username, @RequestParam(value="theUrl") String theUrl, @RequestParam(value="remember") boolean remember)  {
+		String result = null;
+	
+		System.out.println ("[/resetpassword] username is " + username);
+		System.out.println ("[/resetpassword] theUrl is " + theUrl);
+		User user = userService.getUserByUsername(username);
+		
+		if (user != null) {
+			// got the user, send the email
+			String email = user.getEmail();
+			result = "Instructions on how to reset your password have been emailed to you. ";
+			
+			String url = "http://localhost:8080";
+			
+			// get the first part of the url
+			int ipos = theUrl.toLowerCase().indexOf("/biominer");
+			if (ipos != -1) {
+				url = theUrl.substring(0, ipos);
+				System.out.println ("[/resetpassword] url is " + url);
+			}
+			
+			long GUID_EXPIRATION = 1800000;  //30 minutes
+            UUID uuid = UUID.randomUUID();
+            Timestamp ts = new Timestamp(System.currentTimeMillis() + GUID_EXPIRATION);
+			
+            // update the user with the guid and guidExpiration
+			String guid = uuid.toString();   //"d9eb3fa2-171c-4045-b0ac-9b431a072ea2";
+            user.setGuid(guid);
+            user.setGuidExpiration(ts);
+            
+            // flush to database
+            long userId = user.getIdUser();
+            userService.updateUser(userId, user);
+            
+			String body = "A change of password has been requested for the BioMiner account associated with this email.\n\n" +
+			              "Please follow this link to change your password: " + url + "/biominer/#/changepassword?guid=" + guid +
+			              "\n\n" + "This link will expire in 30 minutes.\n";
+			String subject = "Reset BioMiner Password";
+			
+			String status = MailUtil.sendMail(email,body,subject);
+			if (status != null)
+			{
+				result = "Unable to send password reset instructions, error: " + status;
+			}
+		} else {
+			result = "Username does not exist.";
+		}	
+		
+		System.out.println ("[/resetpassword] returning result: " + result);
+		return result;
+	}
+	
+	@RequestMapping(value="/changepassword",method=RequestMethod.POST)
+	public @ResponseBody String changepassword(@RequestParam(value="username") String username, @RequestParam(value="password") String password, @RequestParam(value="passwordconfirm") String passwordconfirm, @RequestParam(value="guid") String guid, @RequestParam(value="remember") boolean remember)  {
+		String result = null;
+	
+		System.out.println ("[/changepassword] username is " + username);
+		System.out.println ("[/changepassword] guid is " + guid);
+		
+		// did we get a good guid?
+		if (guid == null || guid.equals("")) {
+			result = "Parameter error, guid is missing.";
+			return result;
+		}
+		
+		// password match?
+		if (!password.equals(passwordconfirm)) {
+			result = "Passwords do not match.";
+			return result;
+		}
+		
+		User user = userService.getUserByUsername(username);
+		
+		if (user != null) {
+			
+			String userguid = user.getGuid();
+			Timestamp userguidExpiration = user.getGuidExpiration();
+			
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            
+            if (userguidExpiration == null || userguidExpiration.before(currentTime)) {
+            	result = "Password reset request has expired. Try reset password again.";
+ 
+                user.setGuid(null);
+                user.setGuidExpiration(null);
+                
+                // flush to database
+                long userId = user.getIdUser();
+                userService.updateUser(userId, user);
+
+                return result;
+            }
+            
+            if (userguid == null || !userguid.equals(guid)) {
+            	result = "Guid don't match. Try reset password again.";
+ 
+                user.setGuid(null);
+                user.setGuidExpiration(null);
+                
+                // flush to database
+                long userId = user.getIdUser();
+                userService.updateUser(userId, user);
+
+                return result;
+            }
+            
+			
+            // update the user with the new password
+        	String salt = userController.createSalt();
+        	user.setSalt(salt);
+        	
+        	String npass = userController.createPassword(password, salt);
+        	user.setPassword(npass);
+            
+            user.setGuid(null);
+            user.setGuidExpiration(null);
+            
+            // flush to database
+            long userId = user.getIdUser();
+            userService.updateUser(userId, user);
+            
+            result = "Password successfully changed.";
+            
+		} else {
+			result = "Username does not exist.";
+		}	
+		
+		System.out.println ("[/changepassword] returning result: " + result);
+		return result;
+	}
 	
 	@RequestMapping(value="/logout",method=RequestMethod.POST)
 	public @ResponseBody void logout() {
@@ -80,5 +229,8 @@ public class SecurityController {
 		
 		return lm;
 	}
+	
+	
+	
 
 }
